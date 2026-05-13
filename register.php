92f66e70361e5a62643bd3dccecb4998f58920d3
Dalal721 <dalmalki7@icloud.com>
2026-05-13 21:43:43 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,29 @@
+<form method="post">
+
+    <div class="input-group">
+        <label for="name">الاسم</label>
+        <input type="text"
+               name="name"
+               id="name"
+               required>
+    </div>
+
+    <div class="input-group">
+        <label for="email">البريد الإلكتروني</label>
+        <input type="email"
+               name="email"
+               id="email"
+               required>
+    </div>
+
+    <div class="input-group">
+        <label for="password">كلمة المرور</label>
+        <input type="password"
+               name="password"
+               id="password"
+               required>
+    </div>
+
+    <input type="submit" value="إنشاء حساب">
+
+</form>
